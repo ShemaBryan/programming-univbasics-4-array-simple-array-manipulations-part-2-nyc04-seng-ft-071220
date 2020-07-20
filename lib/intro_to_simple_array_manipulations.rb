8808727e175cce26591e00616d2b8693e148293b
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-07-20 19:29:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,19 +1,16 @@
 def using_concat(array_one, array_two)
-    new array = array_one.concat(array_two)
+    array_one.concat(array_two)
 end
 
 def using_insert(array, element)
-    array = []
-    array.insert(4,element)
+    array.insert(4, element)
 end
 
 def using_uniq(array)
-   array = []
-   new_array = array.uniq
+    array.uniq
 end
 
 def using_flatten(array)
-    array = [ [] ]
     array.flatten
 end
 
@@ -22,7 +19,5 @@ def using_delete(array, string)
 end
 
 def using_delete_at(array, integer)
-     array = []
-     integer = 2
-     array.delete_at(integer)
+    array.delete_at(integer)
 end
